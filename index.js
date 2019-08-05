bb15b611f3ec933c4e9fa389b37e1a72a1fc1043
cd <azzamathar@hotmail.com>
2019-08-05 03:42:23 +0000

    initial functions

---
+++ index.js
@@ -0,0 +1,7 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printPassTimes(passTimes);
+});
